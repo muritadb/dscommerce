93a17cbd12b1129455082e601c531d9ae0163577
Douglas Murita <douglasb008@gmail.com>
2025-03-13 22:08:00 -0300

    orderitem, orderitempk

--- src/main/java/br/com/muritadb/dscommerce/entities/Order.java
+++ src/main/java/br/com/muritadb/dscommerce/entities/Order.java
@@ -1,6 +1,9 @@
 package br.com.muritadb.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -32,19 +36,13 @@ public class Order {
 
   @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
   private Payment payment;
-  
-  public User getClient() {
-    return client;
-  }
 
-  public void setClient(User client) {
-    this.client = client;
-  }
+  @OneToMany(mappedBy = "id.order")
+  private Set<OrderItem> items = new HashSet<>();
 
   public Order() {
     
   }
-
 
   public Order(Long id, Instant moment, OrderStatus status, User client,Payment payment) {
     this.id = id;
@@ -52,6 +50,18 @@ public class Order {
     this.status = status;
     this.client = client;
     this.payment = payment;
+  }
+
+
+  
+
+
+  public User getClient() {
+    return client;
+  }
+
+  public void setClient(User client) {
+    this.client = client;
   }
 
   public Long getId() {
@@ -107,6 +117,15 @@ public class Order {
     } else if (!moment.equals(other.moment))
       return false;
     return true;
+  }
+
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Product> getProducts() {
+    return items.stream().map(item -> item.getProduct()).toList();
   }
 
 }
